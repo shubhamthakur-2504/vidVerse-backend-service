51740b0a5ff3c150ad5d68a9c9a1080bfd4c4f10
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-03-04 18:04:44 +0530

    feat: config Cloudinary for file uploads

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs'
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async function (localFilePath) {
+    try {
+        if(!localFilePath) return null
+        const res = await cloudinary.uploader.upload(
+            localFilePath,{
+                resource_type:'auto'
+            }
+        ) 
+        console.log("File uploaded on Cloudinary. File Src : "+ res.url);
+        fs.unlinkSync(localFilePath)
+        return res
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export {uploadOnCloudinary}

fc751b412f25f4a53131e979638cac6b57b97f2c
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-10-28 17:26:12 +0530

    fix: fixed the name issue in db

--- src/db/index.js
+++ src/db/index.js
@@ -3,10 +3,15 @@ import { DB_NAME } from "../constants.js";
 
 export const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(`${process.env.MONGODB_URL}${DB_NAME}`);
+        const connection = await mongoose.connect(`${process.env.MONGODB_URL}${DB_NAME}?retryWrites=true&w=majority`, {
+            ssl: true,
+            tls: true,
+            tlsInsecure: false,
+            serverSelectionTimeoutMS: 30000,
+        });
         console.log(`db: mongoose connected to ${connection.connection.host}`);
         console.log(`db: mongoose connected to ${connection.connection.db.databaseName}`);
-        
+
     } catch (error) {
         console.log("db: mongoose connection error::", error);
         process.exit(1);
